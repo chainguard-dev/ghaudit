40217c2ca9effde6e874660f05292179662d21d2
Matt Moore <mattmoor@chainguard.dev>
2024-03-15 18:56:04 -0700

    Add a check for default permissions.
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ pkg/org/defaultpermissions.go
@@ -0,0 +1,24 @@
+// Copyright 2024 Chainguard, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package org
+
+import (
+	"github.com/chainguard-dev/ghaudit/pkg/repo"
+	"github.com/google/go-github/v60/github"
+	"github.com/spf13/cobra"
+)
+
+func defaultPermissions(ghc *github.Client, org *string) *cobra.Command {
+	rm := NewRepoMapper(ghc, org, repo.DefaultPermissions)
+
+	return &cobra.Command{
+		Use:           "default-permissions",
+		Short:         "Audit the default permissions.",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return rm.Execute(cmd.Context())
+		},
+	}
+}
